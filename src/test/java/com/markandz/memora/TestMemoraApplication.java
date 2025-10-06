ae884b0d38c2a26c5541e6fccf0cd7d733d38758
Mark <mark.andzejevski01@gmail.com>
2025-10-06 14:21:42 +0300

    Initial Spring Boot application

---
+++ src/test/java/com/markandz/memora/TestMemoraApplication.java
@@ -0,0 +1,11 @@
+package com.markandz.memora;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestMemoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(MemoraApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
